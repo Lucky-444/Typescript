21a394a6213dd43ec3e803ec6f81fa6a9f9e2d20
Lucky <sashankswain6@gmail.com>
2026-01-13 01:32:56 +0530

    Add initial project files including .gitignore, README, package.json, and TypeScript configuration

---
+++ src/typesInTS.ts
@@ -0,0 +1,17 @@
+let drink  = "chai";
+// drink = 4; showing error 
+let cups = Math.random() > 0.5 ? 10 : '5';
+
+let channel = "Lucky"
+
+// channel = 12231c ;
+//shoing error due to assign already assigned value with a new value to it
+
+
+let chaiFlavour: string  = "Masala Munch"
+chaiFlavour = "Ginger Masala" ; //chalega
+// chaiFlavour = 2; nahi chalega
+
+let order:boolean = false;
+
+
